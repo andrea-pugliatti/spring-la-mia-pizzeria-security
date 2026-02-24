ac839474227f816da638ebd91d4eab6799c521f4
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-02-24 20:15:39 +0100

    add PizzaService and use it in PizzaService

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_webapi/controllers/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_webapi/controllers/PizzaController.java
@@ -1,13 +1,8 @@
 package org.lessons.java.spring_la_mia_pizzeria_webapi.controllers;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.lessons.java.spring_la_mia_pizzeria_webapi.models.Offer;
 import org.lessons.java.spring_la_mia_pizzeria_webapi.models.Pizza;
-import org.lessons.java.spring_la_mia_pizzeria_webapi.repositories.IngredientRepository;
-import org.lessons.java.spring_la_mia_pizzeria_webapi.repositories.OfferRepository;
-import org.lessons.java.spring_la_mia_pizzeria_webapi.repositories.PizzaRepository;
+import org.lessons.java.spring_la_mia_pizzeria_webapi.services.PizzaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,78 +19,58 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 @RequestMapping("/pizzas")
 public class PizzaController {
-    private PizzaRepository pizzaRepository;
-    private OfferRepository offerRepository;
-    private IngredientRepository ingredientRepository;
+    private PizzaService pizzaService;
 
-    public PizzaController(
-            PizzaRepository pizzaRepository,
-            OfferRepository offerRepository,
-            IngredientRepository ingredientRepository) {
-        this.pizzaRepository = pizzaRepository;
-        this.offerRepository = offerRepository;
-        this.ingredientRepository = ingredientRepository;
+    public PizzaController(PizzaService pizzaService) {
+        this.pizzaService = pizzaService;
     }
 
     @GetMapping
     public String index(Model model, @RequestParam(value = "q", required = false) String name) {
-        List<Pizza> list;
-
-        if (name == null) {
-            list = pizzaRepository.findAll();
-        } else {
-            list = pizzaRepository.findByNameContaining(name);
-        }
-
-        model.addAttribute("pizzas", list);
+        model.addAttribute("pizzas", pizzaService.findAllOrByNameContaining(name));
         model.addAttribute("query", name);
-
         return "pizzas/index";
     }
 
     @GetMapping("/{id}")
     public String show(Model model, @PathVariable("id") Integer pizzaId) {
-        Optional<Pizza> pizza = pizzaRepository.findById(pizzaId);
-
-        if (pizza.isEmpty()) {
+        if (!pizzaService.existsById(pizzaId)) {
             return "redirect:/pizzas";
         }
 
-        model.addAttribute("pizza", pizza.get());
-        model.addAttribute("offers", pizza.get().getOffers());
-        model.addAttribute("ingredients", pizza.get().getIngredients());
+        Pizza pizza = pizzaService.getById(pizzaId);
+        model.addAttribute("pizza", pizza);
+        model.addAttribute("offers", pizza.getOffers());
+        model.addAttribute("ingredients", pizza.getIngredients());
         return "pizzas/show";
     }
 
     @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("pizza", new Pizza());
-        model.addAttribute("ingredients", ingredientRepository.findAll());
+        model.addAttribute("ingredients", pizzaService.findAllIngredients());
         return "pizzas/create";
     }
 
     @PostMapping("/create")
     public String store(@Valid @ModelAttribute("pizza") Pizza pizzaForm, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
-            model.addAttribute("ingredients", ingredientRepository.findAll());
+            model.addAttribute("ingredients", pizzaService.findAllIngredients());
             return "/pizzas/create";
         }
 
-        pizzaRepository.save(pizzaForm);
+        pizzaService.save(pizzaForm);
         return "redirect:/pizzas";
     }
 
     @GetMapping("/edit/{id}")
     public String edit(Model model, @PathVariable("id") Integer pizzaId) {
-        Optional<Pizza> pizza = pizzaRepository.findById(pizzaId);
-        model.addAttribute("ingredients", ingredientRepository.findAll());
-
-        if (pizza.isEmpty()) {
+        model.addAttribute("ingredients", pizzaService.findAllIngredients());
+        if (!pizzaService.existsById(pizzaId)) {
             model.addAttribute("pizza", new Pizza());
             return "redirect:/pizzas/create";
         }
-
-        model.addAttribute("pizza", pizza.get());
+        model.addAttribute("pizza", pizzaService.getById(pizzaId));
         return "/pizzas/edit";
     }
 
@@ -103,43 +78,27 @@ public class PizzaController {
     public String update(@Valid @ModelAttribute("pizza") Pizza pizzaForm, BindingResult bindingResult,
             Model model) {
         if (bindingResult.hasErrors()) {
-            model.addAttribute("ingredients", ingredientRepository.findAll());
+            model.addAttribute("ingredients", pizzaService.findAllIngredients());
             return "/pizzas/edit";
         }
-
-        pizzaRepository.save(pizzaForm);
-
+        pizzaService.edit(pizzaForm);
         return "redirect:/pizzas";
     }
 
     @PostMapping("/delete/{id}")
     public String delete(Model model, @PathVariable("id") Integer pizzaId) {
-        Pizza pizza = pizzaRepository.findById(pizzaId).get();
-
-        List<Offer> offers = offerRepository.findByPizza(pizza);
-
-        for (Offer offer : offers) {
-            offerRepository.delete(offer);
-        }
-
-        pizzaRepository.deleteById(pizzaId);
-
+        pizzaService.deleteById(pizzaId);
         return "redirect:/pizzas";
     }
 
     @GetMapping("/{id}/create-offer")
-    public String getCreateOffer(@PathVariable("id") Integer pizzaId, Model model) {
-        Optional<Pizza> pizza = pizzaRepository.findById(pizzaId);
-
-        if (pizza.isEmpty()) {
+    public String createOffer(@PathVariable("id") Integer pizzaId, Model model) {
+        if (!pizzaService.existsById(pizzaId)) {
             return "redirect:/pizzas";
         }
-
         Offer newOffer = new Offer();
-        newOffer.setPizza(pizza.get());
-
+        newOffer.setPizza(pizzaService.getById(pizzaId));
         model.addAttribute("offer", newOffer);
-
         return "offers/create";
     }
 
---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_webapi/services/PizzaService.java
@@ -0,0 +1,80 @@
+package org.lessons.java.spring_la_mia_pizzeria_webapi.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.lessons.java.spring_la_mia_pizzeria_webapi.models.Ingredient;
+import org.lessons.java.spring_la_mia_pizzeria_webapi.models.Offer;
+import org.lessons.java.spring_la_mia_pizzeria_webapi.models.Pizza;
+import org.lessons.java.spring_la_mia_pizzeria_webapi.repositories.IngredientRepository;
+import org.lessons.java.spring_la_mia_pizzeria_webapi.repositories.OfferRepository;
+import org.lessons.java.spring_la_mia_pizzeria_webapi.repositories.PizzaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PizzaService {
+    private PizzaRepository pizzaRepository;
+    private OfferRepository offerRepository;
+    private IngredientRepository ingredientRepository;
+
+    public PizzaService(
+            PizzaRepository pizzaRepository,
+            OfferRepository offerRepository,
+            IngredientRepository ingredientRepository) {
+        this.pizzaRepository = pizzaRepository;
+        this.offerRepository = offerRepository;
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    public Pizza getById(Integer id) {
+        Optional<Pizza> pizza = pizzaRepository.findById(id);
+        if (pizza.isEmpty()) {
+            // throw new NameNotFoundException("Pizza not found");
+        }
+
+        return pizza.get();
+    }
+
+    public List<Pizza> findAll() {
+        return pizzaRepository.findAll();
+    }
+
+    public List<Pizza> findByNameContaining(String name) {
+        return pizzaRepository.findByNameContaining(name);
+    }
+
+    public List<Pizza> findAllOrByNameContaining(String name) {
+        List<Pizza> list;
+
+        if (name == null) {
+            list = findAll();
+        } else {
+            list = findByNameContaining(name);
+        }
+        return list;
+    }
+
+    public Boolean existsById(Integer id) {
+        return pizzaRepository.existsById(id);
+    }
+
+    public Pizza save(Pizza pizza) {
+        return pizzaRepository.save(pizza);
+    }
+
+    public Pizza edit(Pizza pizza) {
+        return pizzaRepository.save(pizza);
+    }
+
+    public void deleteById(Integer id) {
+        Pizza pizza = getById(id);
+        for (Offer offer : pizza.getOffers()) {
+            offerRepository.delete(offer);
+        }
+        pizzaRepository.delete(pizza);
+    }
+
+    public List<Ingredient> findAllIngredients() {
+        return ingredientRepository.findAll();
+    }
+}
